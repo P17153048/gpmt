bdea71cc6441be0ff2fdc5e67e1bd1901b91b75c
boss.hk@hotmail.co.uk <boss.hk@hotmail.co.uk>
2020-05-25 15:00:02 +0530

    Implemented feature to send new message

--- modules/messages.php
+++ modules/messages.php
@@ -5,3 +5,15 @@ function get_unread_message_count($user_id){
     $database = new DB();
     return $database->num_rows ('SELECT * FROM messages WHERE status = 0 AND deliver_id = ' . $user_id);
 }
+
+function send_message($sent_id, $deliver_id, $title, $message, $status, $date){
+    $database = new DB();
+    return $database->insert ( 'messages', array(
+        'sent_id' => $sent_id,
+        'deliver_id' => $deliver_id,
+        'title' => $title,
+        'message' => $message,
+        'status' => $status,
+        'date' => $date
+    ) );
+}
--- settings.php
+++ settings.php
@@ -48,7 +48,7 @@ $unread_messages = get_unread_message_count ($user['id']);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Index</title>
+    <title>Settings</title>
     <style>
         main {
             padding-top: 80px;
