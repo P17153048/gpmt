6c315cf456c18b146aaa6e0c4268c9d0bec163e2
boss.hk@hotmail.co.uk <boss.hk@hotmail.co.uk>
2020-05-25 14:30:41 +0530

    Implemented feature to display tasks in calendar

---
+++ calendar.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+require_once './modules/tasks.php';
+$user = $_SESSION['user'];
+$date = null;
+
+if (isset($_GET['date'])) {
+    $date = $_GET['date'];
+} else {
+    $date = date("d-m-Y");
+}
+
+$tasks = get_tasks_by_date ($user['id'], $date);
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Index</title>
+    <style>
+        main {
+            padding-top: 80px;
+        }
+
+    </style>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">GPMT</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="projects.php">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="calendar.php">Calendar</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo $user['email']; ?>
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="settings.php">Settings</a>
+                        <a class="dropdown-item" href="messages.php">Messages <span class="badge badge-danger">4</span></a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="logout.php">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main class="container">
+    <div class="row">
+        <div class="col-12 mb-2">
+            <form class="form-inline" id="form" method="get">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <a href="?date=<?=date('d-m-Y', strtotime('-1 day 00:00'))?>" class="btn btn-primary btn-sm">Yesterday</a>
+                        <a href="?date=<?=date('d-m-Y', strtotime('00:00'))?>" class="btn btn-primary btn-sm">Today</a>
+                        <a href="?date=<?=date('d-m-Y', strtotime('+1 day 00:00'))?>" class="btn btn-primary btn-sm">Tomorrow</a>
+                    </div>
+                    <input type="date" id="date" class="form-control form-control-sm" value="<?=$date?>" >
+                    <input type="hidden" name="date" id="post_date" value="">
+                </div>
+            </form>
+        </div>
+        <?php
+        echo '<div class="col-12"><h3>' . date("d-m-Y", strtotime($date))  . '</h3></div>';
+        ?>
+
+        <div class="col-12 mt-3" id="tasks">
+            <?php
+            foreach ($tasks as $task) {
+                $badge = $task[ 'status' ] == 1 ? 'success' : 'info';
+                echo '<div class="card border-' . $badge . ' mb-2">
+                        <div class="card-body">' .
+                    ( $task[ 'status' ] == 0 ? '<div class="btn-group float-right">
+                                <a href="calendar.php?action=complete_task&task_id=' . $task[ 'id' ] . '" class="btn btn-sm btn-success float-right">Complete</a>
+                                </div>' : '' )
+                    . '<p class="m-0">' . $task[ 'description' ] . '</p>
+                        </div>
+                        
+                    </div>';
+
+
+            }
+            ?>
+
+        </div>
+    </div>
+</main>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+
+<script>
+    $(document).ready(function(){
+        $('#date').change(function(){
+            debugger;
+            let selectedDate = $("#date").val();
+            let postDate = new Date(selectedDate).toLocaleDateString();
+            $('#post_date').val(postDate);
+            $('#form').submit();
+        });
+    });
+</script>
+</body>
+
+</html>
--- modules/tasks.php
+++ modules/tasks.php
@@ -39,3 +39,12 @@ function update_task($task_id, $user_id, $description, $deadline_date){
     $update_where = array( 'id' => $task_id );
     return $database->update ('tasks', $update, $update_where);
 }
+
+function get_tasks_by_date($user_id, $date){
+    $database = new DB();
+    return $database->get_results ( "SELECT t.*, p.title project_title, p.description project_description, p.date_created project_created, p.status project_status, u.f_name, u.l_name, u.username
+                                            FROM tasks t
+                                                INNER JOIN projects p ON p.id = t.project_id
+                                                INNER JOIN users u ON u.id = t.user_id
+                                        	WHERE DATE_FORMAT(FROM_UNIXTIME(deadline_date), '%d-%m-%Y') = '" . $date . "' AND t.user_id = " . $user_id);
+}
